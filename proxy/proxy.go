4e4964087a16e4c21747a8a65e7bbb976b5d69ab
Amaterasy17 <sheb70017@gmail.com>
2021-03-10 23:48:27 +0300

    change protocol? hahahhahaha))))

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,8 +1,8 @@
 package proxy
 
 import (
+	"bufio"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net"
@@ -23,20 +23,49 @@ func (p *Proxy) NewProxy(config string) *Proxy {
 }
 
 func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	newRequest := p.parseNewRequest(r)
+	if newRequest.port == "" {
+		newRequest.port = ":80"
+	} else {
+		newRequest.port = ""
+	}
+	r.RequestURI = ""
+	r.Header.Del("Proxy-Connection")
 	//newRequest := p.parseNewRequest(r)
 	//result := p.MakeRequest(newRequest, r)
 	//w.Write(result)
-	p.HTTPHandler(w, r)
+	conn, err := net.Dial("tcp", newRequest.host + newRequest.port)
+	if err != nil {
+		fmt.Println("error?")
+	}
+
+	reqData := newRequest.method + " " + newRequest.path + " HTTP/1.0\r\n" + "Host: " + newRequest.host + "\r\nUser-Agent: " + newRequest.userAgent + "\r\n\r\n"
+	//reqData := r.Method + " " + r.URL.Path + " HTTP/1.1" + "\r\n\r\n"
+	fmt.Println(reqData)
+	//_, err = fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+	 _, err = fmt.Fprintf(conn, reqData)
+	if err != nil {
+		fmt.Println("kak zaebal")
+	}
+	status := bufio.NewReader(conn)
+	body, err := ioutil.ReadAll(status)
+	if err != nil {
+		fmt.Println("oi")
+	}
+	//fmt.Println(body)
+	fmt.Printf("%s", body)
+	w.Write(body)
+	//p.HTTPHandler(w, r)
 	//http.Client{}
 }
 
 func (p *Proxy) parseNewRequest(r *http.Request) Req {
 	fmt.Println(r.Method)
 	fmt.Println(r.Host)
-	fmt.Println(r.URL.Path)
-	fmt.Println(r.URL.Port())
-	fmt.Println(r.UserAgent())
-	fmt.Println(r.Body)
+	//fmt.Println(r.URL.Path)
+	//fmt.Println(r.URL.Port())
+	//fmt.Println(r.UserAgent())
+	//fmt.Println(r.Body)
 	result, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Fatalln(err)
@@ -97,12 +126,27 @@ func (p *Proxy) HTTPHandler(w http.ResponseWriter, r *http.Request)  {
 	}
 
 	reqData := newRequest.method + " " + newRequest.path + " HTTP/1.1\n" + "Host: " + newRequest.host + "\nUser-Agent: " + newRequest.userAgent + "\n\n"
-	_, err = conn.Write([]byte(reqData))
+	fmt.Println(reqData)
+	fmt.Fprintf(conn, reqData)
+
+	//_, err = conn.Write([]byte(reqData))
+	//if err != nil {
+	//	w.WriteHeader(http.StatusInternalServerError)
+	//	w.Write([]byte("error"))
+	//	return
+	//}
+	defer conn.Close()
+
+	status := bufio.NewReader(conn)
+	body, err := ioutil.ReadAll(status)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("error"))
-		return
+		fmt.Println("oi")
 	}
+	fmt.Printf("%s", body)
+
+	//var readChan chan []byte = make(chan []byte)
+	//p.ReadResponse(conn, readChan)
+	//fmt.Println("After read response")
 	//var reader io.Reader
 	//var read []byte
 	//var readChan chan []byte = make(chan []byte)
@@ -119,13 +163,29 @@ func (p *Proxy) HTTPHandler(w http.ResponseWriter, r *http.Request)  {
 	//	fmt.Println(<-readChan)
 	//}
 
-	conn.SetDeadline(time.Now().Add(time.Millisecond * 100))
+	//conn.SetDeadline(time.Now().Add(time.Millisecond * 100))
 	w.WriteHeader(http.StatusOK)
 	//io.Copy(w, strings.NewReader(string(<-readChan)))
-	io.Copy(w, conn)
-	//w.Write(<-readChan)
-	defer conn.Close()
+	//io.Copy(w, conn)
+	w.Write(body)
 
+}
+
+func (p *Proxy) ReadResponse(conn net.Conn, readChan chan []byte) {
+	fmt.Println("I in read response")
+	var read []byte
+	for {
+		n, err := conn.Read(read)
+		if err != nil {
+			close(readChan)
+			break
+		}
+		if n != 0 {
+			fmt.Println("Reading")
+		}
+		readChan <- read
+	}
+	fmt.Println("I in end response")
 }
 
 type Req struct {
