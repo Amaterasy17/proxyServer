adf25a851e0ba3da98e063bcc98b4bb509953d20
Amaterasy17 <sheb70017@gmail.com>
2021-03-15 21:58:43 +0300

    have done proxy on http/https

--- main.go
+++ main.go
@@ -1,23 +1,44 @@
 package main
 
 import (
+	"crypto/tls"
+	"flag"
 	"fmt"
-	"proxyServer/proxy"
-
+	"log"
 	"net/http"
+	"proxyServer/proxy"
 )
 
 func main() {
 	var p proxy.Proxy
-	p.NewProxy("memes")
+	p.NewProxy("config")
 
+	var pem, key, proto string
+	flag.StringVar(&pem, "pem", "RootCA.pem", "")
+	flag.StringVar(&key, "key", "RootCA.key", "")
+	flag.StringVar(&proto, "proto", "http", "")
+	flag.Parse()
+
+	if proto != "http" && proto != "https" {
+		log.Println("Protocol must be either http or https")
+		return
+	}
 
 
 	fmt.Println("Starting server on localhost:8080")
 	addr := ":8080"
-	err := http.ListenAndServe(addr, &p)
-	if err != nil {
-		fmt.Println("error of starting server")
+	server := &http.Server{
+		Addr: addr,
+		Handler: &p,
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
+	}
+
+	//
+	if proto == "http" {
+		log.Fatal(server.ListenAndServe())
+	} else {
+		log.Fatal(server.ListenAndServeTLS(pem, key))
 	}
 
 }
+
--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,8 +1,8 @@
 package proxy
 
 import (
-	"bufio"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
@@ -10,62 +10,25 @@ import (
 	"time"
 )
 
-var res = []byte("<html>\n<head><title>301 Moved Permanently</title></head>\n<body bgcolor=\"white\">\n<center><h1>301 Moved Permanently</h1></center>\n<hr><center>nginx/1.14.1</center>\n</body>\n</html>\n")
-
 type Proxy struct {
 	Config string
 }
-
-
 
 func (p *Proxy) NewProxy(config string) *Proxy {
 	return &Proxy{Config: config}
 }
 
 func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	newRequest := p.parseNewRequest(r)
-	if newRequest.port == "" {
-		newRequest.port = ":80"
+	if r.Method == http.MethodConnect {
+		p.HTTPSHandler(w, r)
 	} else {
-		newRequest.port = ""
+		p.HTTPHandler(w, r)
 	}
-	r.RequestURI = ""
-	r.Header.Del("Proxy-Connection")
-	//newRequest := p.parseNewRequest(r)
-	//result := p.MakeRequest(newRequest, r)
-	//w.Write(result)
-	conn, err := net.Dial("tcp", newRequest.host + newRequest.port)
-	if err != nil {
-		fmt.Println("error?")
-	}
-
-	reqData := newRequest.method + " " + newRequest.path + " HTTP/1.0\r\n" + "Host: " + newRequest.host + "\r\nUser-Agent: " + newRequest.userAgent + "\r\n\r\n"
-	//reqData := r.Method + " " + r.URL.Path + " HTTP/1.1" + "\r\n\r\n"
-	fmt.Println(reqData)
-	//_, err = fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
-	 _, err = fmt.Fprintf(conn, reqData)
-	if err != nil {
-		fmt.Println("kak zaebal")
-	}
-	status := bufio.NewReader(conn)
-	body, err := ioutil.ReadAll(status)
-	if err != nil {
-		fmt.Println("oi")
-	}
-	//fmt.Println(body)
-	fmt.Printf("%s", body)
-	w.Write(body)
-	//p.HTTPHandler(w, r)
-	//http.Client{}
 }
 
 func (p *Proxy) parseNewRequest(r *http.Request) Req {
 	fmt.Println(r.Method)
 	fmt.Println(r.Host)
-	//fmt.Println(r.URL.Path)
-	//fmt.Println(r.URL.Port())
-	//fmt.Println(r.UserAgent())
-	//fmt.Println(r.Body)
 	result, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Fatalln(err)
@@ -110,65 +73,18 @@ func (p *Proxy) MakeRequest(req Req, r *http.Request) []byte {
 }
 
 func (p *Proxy) HTTPHandler(w http.ResponseWriter, r *http.Request)  {
-	newRequest := p.parseNewRequest(r)
-	if newRequest.port == "" {
-		newRequest.port = ":80"
-	} else {
-		newRequest.port = ""
-	}
-	r.RequestURI = ""
-	r.Header.Del("Proxy-Connection")
-
-	conn, err := net.Dial("tcp", newRequest.host + newRequest.port)
+	fmt.Println(r.Method)
+	fmt.Println(r.URL)
+	resp, err := http.DefaultTransport.RoundTrip(r)
 	if err != nil {
-		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	defer resp.Body.Close()
 
-	reqData := newRequest.method + " " + newRequest.path + " HTTP/1.1\n" + "Host: " + newRequest.host + "\nUser-Agent: " + newRequest.userAgent + "\n\n"
-	fmt.Println(reqData)
-	fmt.Fprintf(conn, reqData)
-
-	//_, err = conn.Write([]byte(reqData))
-	//if err != nil {
-	//	w.WriteHeader(http.StatusInternalServerError)
-	//	w.Write([]byte("error"))
-	//	return
-	//}
-	defer conn.Close()
-
-	status := bufio.NewReader(conn)
-	body, err := ioutil.ReadAll(status)
-	if err != nil {
-		fmt.Println("oi")
-	}
-	fmt.Printf("%s", body)
-
-	//var readChan chan []byte = make(chan []byte)
-	//p.ReadResponse(conn, readChan)
-	//fmt.Println("After read response")
-	//var reader io.Reader
-	//var read []byte
-	//var readChan chan []byte = make(chan []byte)
-	//n, err := conn.Read(read)
-	//if err != nil {
-	//	w.WriteHeader(http.StatusInternalServerError)
-	//	w.Write([]byte("error"))
-	//	return
-	//}
-	//if n != 0 {
-	//	readChan <- read
-	//} else {
-	//	fmt.Println("fuck")
-	//	fmt.Println(<-readChan)
-	//}
-
-	//conn.SetDeadline(time.Now().Add(time.Millisecond * 100))
-	w.WriteHeader(http.StatusOK)
-	//io.Copy(w, strings.NewReader(string(<-readChan)))
-	//io.Copy(w, conn)
-	w.Write(body)
-
+	p.CopyHeader(w.Header(), resp.Header)
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
 }
 
 func (p *Proxy) ReadResponse(conn net.Conn, readChan chan []byte) {
@@ -186,6 +102,47 @@ func (p *Proxy) ReadResponse(conn net.Conn, readChan chan []byte) {
 		readChan <- read
 	}
 	fmt.Println("I in end response")
+}
+
+func (p *Proxy) HTTPSHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Method)
+	fmt.Println(r.URL)
+
+	destination, err := net.DialTimeout("tcp", r.Host, 10*time.Second)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	hijacker, ok := w.(http.Hijacker)
+	if !ok {
+		fmt.Println(ok)
+		http.Error(w, "Hijacking not supported", http.StatusInternalServerError)
+		return
+	}
+	client, _, err := hijacker.Hijack()
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	go p.TransferData(destination, client)
+	go p.TransferData(client, destination)
+}
+
+func (p *Proxy) TransferData(destination io.WriteCloser, source io.ReadCloser) {
+	defer destination.Close()
+	defer source.Close()
+	io.Copy(destination, source)
+}
+
+func (p *Proxy) CopyHeader(dest, src http.Header) {
+	for key, values := range src {
+		for _, value := range values {
+			dest.Add(key, value)
+		}
+	}
 }
 
 type Req struct {
